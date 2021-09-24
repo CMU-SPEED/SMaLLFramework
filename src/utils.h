345440753031d236842ee146d6b89b216c541328
UpasanaSridhar <upasanas@cmu.edu>
2021-09-24 14:58:30 -0400

    separated general utilties from torch utilities

--- src/utils.h
+++ src/utils.h
@@ -1,11 +1,95 @@
-// A set of functions to help interface between Libtorch and this codebase
+// A set of functions to enable testing
+// We have initialization, allocation logging and equivalence checking  
+
+//Allocation
+
+float * alloc (uint32_t numel)
+{
+  float *ptr_dc;
+
+  int ret = posix_memalign((void **)&ptr_dc, 4096, numel * sizeof(float));
+
+  if (ret)
+  {
+    return NULL;
+  }
+  return ptr_dc;
+}
+
+void init(float * ptr, uint32_t numel)
+{
+  float * cur_ptr = ptr;
+  for(uint32_t i = 0 ; i < numel ; i++)
+  {
+    *(cur_ptr) = ((float) rand()/ RAND_MAX) + 1; 
+  }
+}
+
+void init_ones(float *ptr, uint32_t numel)
+{
+  float *cur_ptr = ptr;
+  for (uint32_t i = 0; i < numel; i++)
+  {
+    *(cur_ptr) = 1;
+  }
+}
+
+void init_norm(float *ptr, uint32_t numel, uint32_t C_o)
+{
+  float *cur_ptr = ptr;
+  float norm = (1.0*C_o)/(1.0*numel);
+  for (uint32_t i = 0; i < numel; i++)
+  {
+    *(cur_ptr) = norm;
+  }
+}
+bool equals(uint32_t numel, float *unfused, float *fused, float tolerance = 1e-8)
+{
+  bool check = 1;
+  float *unfused_ptr = unfused;
+  float *fused_ptr = fused;
+
+  for (uint32_t i = 0; i < numel; i++)
+  {
+
+    float diff = *(fused_ptr) - *(unfused_ptr);
+    if(fabs( diff > tolerance))
+    {
+      printf("%d %.4f %.4f %.4f\n", numel/C_ob, *(fused_ptr), *(unfused_ptr),  diff);
+      check = 0; 
+      // return 0;
+    }
+
+    unfused_ptr++;
+    fused_ptr++;
+  }
+  return check;
+}
+
+void write_results(std::string file, std::vector<std::vector<uint64_t>> implementations)
+{
+  std::string path = "Results/";
+  std::string path_to_log_file = path + file;
+  // char * file_path = (char *) path_to_log_file;
+  FILE *logFile = fopen(path_to_log_file.c_str(), "w");
+  for (const auto &e : implementations)
+  {
+    for (const auto &t : e)
+    {
+      // logFile << t << "\n";
+      fprintf(logFile, "%lu\n", t);
+    }
+    // logFile << "\n";
+    fprintf(logFile, "\n");
+  }
+  fclose(logFile);
+}
 
 //Utility functions to Interface between libtorch and C++
 
 
-
-// allocate a page aligned flatbuffer of size Tensor.numel()
-// Copy the dimensions of Tensor to a C++ vector to iterate over
+allocate a page aligned flatbuffer of size Tensor.numel()
+Copy the dimensions of Tensor to a C++ vector to iterate over
 float * alloc_dc(torch::Tensor t, std::vector<uint32_t> &dimensions)
 {
   uint32_t total_elements = 1;
@@ -44,8 +128,8 @@ float * alloc_dc_pooling_aware(torch::Tensor t, std::vector<uint32_t> &dimension
 }
 
 
-// copey elements of the torch tensor into the flat buffer allocated above in the
-// direct conv data layout
+copey elements of the torch tensor into the flat buffer allocated above in the
+direct conv data layout
 int copy_torch2dc(torch::Tensor t,
                       char type, // 'i' = input, 'f' = filter, 'o'  = output
                       std::vector<uint32_t> dim_sizes,
@@ -292,136 +376,119 @@ bool check_block_eqivalence(uint32_t block_size,
  return check;
 }
 
-void write_results(std::string file, std::vector<std::vector<uint64_t>> implementations)
+
+
+bool check_eqivalence(torch::Tensor t,
+                      char type,
+                      std::vector<uint32_t> dim_sizes,
+                      float * dc_array, float tolerance = 1e-8)
 {
-  std::string path = "Results/";
-  std::string path_to_log_file = path + file;
-  // char * file_path = (char *) path_to_log_file;
-  FILE *logFile = fopen(path_to_log_file.c_str(), "w");
-  for (const auto &e : implementations)
-  {
-    for (const auto &t : e)
-    {
-      // logFile << t << "\n";
-      fprintf(logFile, "%lu\n", t);
-    }
-    // logFile << "\n";
-    fprintf(logFile, "\n");
-  }
-  fclose(logFile);
+ bool check = 1;
+ float * flat_t = t.data_ptr<float>();
+ // uint32_t dim5, dim_4, dim_3, dim_2, dim_1, dim_0;
+ uint32_t dim_3, dim_2, dim_1, dim_0;
+
+ uint32_t ip_block, op_block;
+
+ dim_3 = dim_sizes[0]; //C_o
+ dim_2 = dim_sizes[1]; //C_i
+ dim_1 = dim_sizes[2]; //H_f
+ dim_0 = dim_sizes[3]; //W_f
+
+
+ if(type=='i'){
+   //input
+   ip_block = C_ib;
+   op_block = 1;
+ }
+ else if(type=='o'){
+   //output
+   ip_block = C_ob;
+   op_block = 1;
+
+ }
+
+ else if(type == 'f'){
+   //filter
+   ip_block = C_ib;
+   op_block = C_ob;
+
+ }
+ else{
+   return 0;
+ }
+ // copying
+ uint32_t offset = 0;
+ for(uint32_t g = 0; g < dim_3; g+= op_block){
+   uint32_t g_offset =  g*dim_2*dim_1*dim_0;
+   for(uint32_t h = 0; h < dim_2; h+= ip_block){
+     uint32_t h_offset = h*dim_1*dim_0;
+     for(uint32_t i = 0; i < dim_1; i++){
+       uint32_t i_offset = i*dim_0;
+       for(uint32_t j = 0; j < dim_0; j++){
+         uint32_t j_offset = j;
+         for(uint32_t k = 0; k < ip_block; k++){
+           uint32_t k_offset = k*dim_1*dim_0;
+           for(uint32_t l = 0; l < op_block; l++){
+             int l_offset = l*dim_2*dim_1*dim_0;
+             // printf("offset: %d\n", offset);fflush(0);
+             check &= (fabs(dc_array[offset++]
+                          -
+                       flat_t[g_offset + l_offset   +
+                              h_offset + k_offset   +
+                                         i_offset   +
+                                         j_offset
+                               ])
+
+                           < tolerance);
+          if((fabs(dc_array[offset-1]
+                       -
+                    flat_t[g_offset + l_offset   +
+                           h_offset + k_offset   +
+                                      i_offset   +
+                                      j_offset
+                            ])
+
+                        > tolerance))
+            {
+             printf("%d %.4f %.4f %f\n",offset-1, dc_array[offset-1] ,flat_t[g_offset + l_offset   +
+                                                              h_offset + k_offset   +
+                                                                         i_offset   +
+                                                                         j_offset
+                                                               ],
+                                                               (fabs(dc_array[offset-1]
+                                                                            -
+                                                                         flat_t[g_offset + l_offset   +
+                                                                                h_offset + k_offset   +
+                                                                                           i_offset   +
+                                                                                           j_offset
+                                                                                 ]))
+                   );
+            }
+           }
+         }
+       }
+     }
+   }
+ }
+ return check;
 }
 
-// bool check_eqivalence(torch::Tensor t,
-//                       char type,
-//                       std::vector<uint32_t> dim_sizes,
-//                       float * dc_array, float tolerance = 1e-8)
-// {
-//  bool check = 1;
-//  float * flat_t = t.data_ptr<float>();
-//  // uint32_t dim5, dim_4, dim_3, dim_2, dim_1, dim_0;
-//  uint32_t dim_3, dim_2, dim_1, dim_0;
-//
-//  uint32_t ip_block, op_block;
-//
-//  dim_3 = dim_sizes[0]; //C_o
-//  dim_2 = dim_sizes[1]; //C_i
-//  dim_1 = dim_sizes[2]; //H_f
-//  dim_0 = dim_sizes[3]; //W_f
-//
-//
-//  if(type=='i'){
-//    //input
-//    ip_block = C_ib;
-//    op_block = 1;
-//  }
-//  else if(type=='o'){
-//    //output
-//    ip_block = C_ob;
-//    op_block = 1;
-//
-//  }
-//
-//  else if(type == 'f'){
-//    //filter
-//    ip_block = C_ib;
-//    op_block = C_ob;
-//
-//  }
-//  else{
-//    return 0;
-//  }
-//  // copying
-//  uint32_t offset = 0;
-//  for(uint32_t g = 0; g < dim_3; g+= op_block){
-//    uint32_t g_offset =  g*dim_2*dim_1*dim_0;
-//    for(uint32_t h = 0; h < dim_2; h+= ip_block){
-//      uint32_t h_offset = h*dim_1*dim_0;
-//      for(uint32_t i = 0; i < dim_1; i++){
-//        uint32_t i_offset = i*dim_0;
-//        for(uint32_t j = 0; j < dim_0; j++){
-//          uint32_t j_offset = j;
-//          for(uint32_t k = 0; k < ip_block; k++){
-//            uint32_t k_offset = k*dim_1*dim_0;
-//            for(uint32_t l = 0; l < op_block; l++){
-//              int l_offset = l*dim_2*dim_1*dim_0;
-//              // printf("offset: %d\n", offset);fflush(0);
-//              check &= (fabs(dc_array[offset++]
-//                           -
-//                        flat_t[g_offset + l_offset   +
-//                               h_offset + k_offset   +
-//                                          i_offset   +
-//                                          j_offset
-//                                ])
-//
-//                            < tolerance);
-//           if((fabs(dc_array[offset-1]
-//                        -
-//                     flat_t[g_offset + l_offset   +
-//                            h_offset + k_offset   +
-//                                       i_offset   +
-//                                       j_offset
-//                             ])
-//
-//                         > tolerance))
-//             {
-//              printf("%d %.4f %.4f %f\n",offset-1, dc_array[offset-1] ,flat_t[g_offset + l_offset   +
-//                                                               h_offset + k_offset   +
-//                                                                          i_offset   +
-//                                                                          j_offset
-//                                                                ],
-//                                                                (fabs(dc_array[offset-1]
-//                                                                             -
-//                                                                          flat_t[g_offset + l_offset   +
-//                                                                                 h_offset + k_offset   +
-//                                                                                            i_offset   +
-//                                                                                            j_offset
-//                                                                                  ]))
-//                    );
-//             }
-//            }
-//          }
-//        }
-//      }
-//    }
-//  }
-//  return check;
-// }
 
 
+bool interleave_arrays(float * a, float * b, uint32_t length, float * interleaved){
 
-// bool interleave_arrays(float * a, float * b, uint32_t length, float * interleaved){
-//
-//   uint32_t offset = 0;
-//
-//   for(uint32_t i = 0; i < length; i+=16){
-//     for(uint32_t j = 0; j < 16; j++){
-//       interleaved[offset] = a[i+j];
-//       offset++;
-//     }
-//     for(uint32_t j = 0; j < 16; j++){
-//       interleaved[offset] = b[i+j];
-//       offset++;
-//     }
-//   }
-//
-// }
+  uint32_t offset = 0;
+
+  for(uint32_t i = 0; i < length; i+=16){
+    for(uint32_t j = 0; j < 16; j++){
+      interleaved[offset] = a[i+j];
+      offset++;
+    }
+    for(uint32_t j = 0; j < 16; j++){
+      interleaved[offset] = b[i+j];
+      offset++;
+    }
+  }
+
+}
