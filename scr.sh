d133ea3250b7ba20f092552895c0d47d000c17a4
UpasanaSridhar <55990573+UpasanaSridhar@users.noreply.github.com>
2022-03-14 12:50:33 -0400

    Delete scr.sh

--- scr.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-./Experiments/varyK_pool_script.sh ./torch_avg_pool 6 | tee vary_K_l_avgpool_l3_6_mar18.txt
-./Experiments/varyK_pool_script.sh ./torch_avg_pool 2 | tee vary_K_l_avgpool_l3_2_mar18.txt
-./Experiments/varyK_pool_script.sh ./torch_avg_pool 4 | tee vary_K_l_avgpool_l3_4_mar18.txt
