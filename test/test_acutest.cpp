8551bcec02c12ee561e96f3a7ebb372a38065097
Scott McMillan <smcmillan@sei.cmu.edu>
2022-08-08 15:18:46 -0700

    Demo version of unit test file.

---
+++ test/test_acutest.cpp
@@ -0,0 +1,39 @@
+//-----------------------------------------------------------------------------
+// test/test_acutest.cpp:  test cases for xxx
+//-----------------------------------------------------------------------------
+
+// LAGraph, (c) 2022 by the Authors, All Rights Reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// See additional acknowledgments in the LICENSE file,
+
+//-----------------------------------------------------------------------------
+
+//#include <LAGraph.h>
+
+#include <acutest.h>
+//#include <params.h>
+//#include <intrinsics.h>
+
+//****************************************************************************
+void test_dummy(void)
+{
+    TEST_MSG("Testing equality %d", 42);
+    TEST_CHECK(42 == 42);
+    //BOOST_CHECK_EQUAL(42, 42);
+}
+
+//****************************************************************************
+void test_dummy_fails(void)
+{
+    TEST_CHECK(42 == 0);
+    //BOOST_CHECK_EQUAL(42, 0);
+}
+
+//****************************************************************************
+//****************************************************************************
+TEST_LIST = {
+    {"dummy", test_dummy},
+    {"dummy_fails", test_dummy_fails},
+    {NULL, NULL}
+};
