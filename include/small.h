d1e4dcd5e6ddb3d2d54ceece89c9a376d8e2080b
Scott McMillan <smcmillan@sei.cmu.edu>
2023-03-01 14:44:35 -0800

    Removing old header comment.

--- include/small.h
+++ include/small.h
@@ -1,3 +1,4 @@
+//****************************************************************************
 // SMaLL, Software for Machine Learning Libraries
 // Copyright 2023 by The SMaLL Contributors, All Rights Reserved.
 // SPDX-License-Identifier: BSD-3-Clause
@@ -7,32 +8,7 @@
 // Contributors.txt for a full list of contributors. Created, in part, with
 // funding and support from the U.S. Government (see Acknowledgments.txt file).
 // DM23-0126
-
-/*
- * SMaLL framework
- *
- * Copyright 2022 Carnegie Mellon University and Authors.
- *
- * THIS MATERIAL WAS PREPARED AS AN ACCOUNT OF WORK SPONSORED BY AN AGENCY OF
- * THE UNITED STATES GOVERNMENT.  NEITHER THE UNITED STATES GOVERNMENT NOR THE
- * UNITED STATES DEPARTMENT OF ENERGY, NOR THE UNITED STATES DEPARTMENT OF
- * DEFENSE, NOR CARNEGIE MELLON UNIVERSITY, NOR ANY OF THEIR
- * EMPLOYEES, NOR ANY JURISDICTION OR ORGANIZATION THAT HAS COOPERATED IN THE
- * DEVELOPMENT OF THESE MATERIALS, MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR
- * ASSUMES ANY LEGAL LIABILITY OR RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS,
- * OR USEFULNESS OR ANY INFORMATION, APPARATUS, PRODUCT, SOFTWARE, OR PROCESS
- * DISCLOSED, OR REPRESENTS THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED
- * RIGHTS.
- *
- * Released under a BSD-style license, please see LICENSE file or contact
- * permission@sei.cmu.edu for full terms.
- *
- * [DISTRIBUTION STATEMENT A] This material has been approved for public release
- * and unlimited distribution.  Please see Copyright notice for non-US
- * Government use and distribution.
- *
- * DMxx-xxxx
- */
+//****************************************************************************
 
 #pragma once
 
