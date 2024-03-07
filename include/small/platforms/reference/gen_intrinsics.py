8840fd99d63afca107a65e8e53c7238162512034
Delaynie <delaynieym@outlook.com>
2024-03-07 13:05:39 -0500

    first draft of conv_tile for row-major refreshing B register
    it's a copy of the col-major version,
    so if there are off-by-one bugs in that version, they are here as well

--- include/small/platforms/reference/gen_intrinsics.py
+++ include/small/platforms/reference/gen_intrinsics.py
@@ -82,9 +82,44 @@ def gen_conv_tile_refresh_col_major_b_reg():
                     # this is not correct assembly syntax at all
 
 
-"""
 def gen_conv_tile_refresh_row_major_b_reg():
-Uses one register for values in the B matrix.
-Refreshes thsoe values iterating through cols in B first, then rows.
+    """Uses one register for values in the B matrix.
+    Refreshes thsoe values iterating through cols in B first, then rows."""
+    print("#define FLOAT_CONV_TILE_C(W_stride, a, b, W_ob, C_ob)\\")
+
+    # declaring registers for matrix A
+    for i in range(FLOAT_W_ob):
+        print(f"float32x4_t a_{i};\\")
+
+    # declaring registers for matrix B
+    # (only using one register for matrix B)
+    for i in range(1):
+        print(f"float32x4_t b_{i};\\")
+
+    # TODO difference between vld1q_dup_f32 and vld1q_f32?
+
+    # filling A registers
+    for i in range(FLOAT_W_ob):
+        print(f"a_{i} = vld1q_dup_f32(a + {i} * W_stride);\\")
+
+    for b_row in range(FLOAT_UNROLL):
+        # this loop controls which col of matrix B gets put into B's register upon refresh
+        # refresh B register to be the next row at the beginning of each iteration of this loop
+        for b_col in range(FLOAT_C_ob):
+            print(f"b_{0} = vld1q_f32(b + {b_row} * C_ob + {b_col} * FLOAT_SIMD);\\")
+
+            for a_col in range(FLOAT_SIMD):
+                # iterating through "cols" in a portion of matrix A.  aka iterating through indices in A's registers
+                # TODO what if the number of cols that A has (FLOAT_UNROLL) is > FLOAT_SIMD?
+                for a_row in range(FLOAT_W_ob):
+                    # iterating through "rows" in matrix A, aka iterating through A's registers (sticking with the same column)
+                    print(
+                        f'__asm__ volatile("fmla c_{a_col}_{a_row}, a_{a_row}[{a_col}], b_{0}");\\'
+                    )
+                    # TODO "fmla [output register] [input regsiter] [input broadcast with index]"
+                    # this is not correct assembly syntax at all
+
+
+"""
 def gen_conv_tile_multiple_b_reg():
 """
