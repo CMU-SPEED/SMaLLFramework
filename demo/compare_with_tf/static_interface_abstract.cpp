1e04bf6b4a4b537acc1392b8d25177a3e52a4cb3
UpasanaSridhar <upasanas@andrew.cmu.edu>
2024-08-27 14:11:46 -0400

    added a folder to compare abstract_layer calls with tf output. Proposed alternative to check_interface.

---
+++ demo/compare_with_tf/static_interface_abstract.cpp
@@ -0,0 +1,33 @@
+//****************************************************************************
+// SMaLL, Software for Machine Learning Libraries
+// Copyright 2023 by The SMaLL Contributors, All Rights Reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// For additional details (including references to third party source code and
+// other files) see the LICENSE file or contact permission@sei.cmu.edu. See
+// Contributors.txt for a full list of contributors. Created, in part, with
+// funding and support from the U.S. Government (see Acknowledgments.txt file).
+// DM23-0126
+//****************************************************************************
+
+#include <math.h>
+#include <stdint.h>
+#include <stdexcept>
+#include <type_traits>
+
+#include <params.h>
+#include <intrinsics.h>
+
+
+#include <small/op_type.hpp>
+
+
+// ================== Public API ====================
+#include <small/utils.hpp>
+#include <small/buffers.hpp>
+
+/// @todo Currently only works with FloatBuffer from reference
+
+//****************************************************************************
+
+//The functions needed to be generated will be added by a python script below
