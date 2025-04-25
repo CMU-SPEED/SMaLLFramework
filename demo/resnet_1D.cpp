97254501399e9363bab0fc7e5aeb93468096e317
UpasanaSridhar <55990573+UpasanaSridhar@users.noreply.github.com>
2025-04-25 10:57:42 -0400

    Fixed a bug in the use of Conv1D in resnet_1D.cpp: height and width swapped (#51)
    Co-authored-by: Scott McMillan <smcmillan@sei.cmu.edu>

--- demo/resnet_1D.cpp
+++ demo/resnet_1D.cpp
@@ -286,7 +286,8 @@ inline void resnet_block(
 #endif
 
     uint32_t o_h = in_dims[1];
-    uint32_t o_w = small::output_dim(in_dims[1] + l_pad_0 + r_pad_0,
+
+    uint32_t o_w = small::output_dim(in_dims[0] + l_pad_0 + r_pad_0,
                                      stride, kernel_size_width);
 
 #if TIME_LAYER
@@ -316,7 +317,7 @@ inline void resnet_block(
     small::AveragePool1D(1, stride,
                          0, 0,
                          input_channels,
-                         in_dims[0], in_dims[1],
+                         in_dims[1], in_dims[0],
                          I,
                          O);
 
@@ -395,7 +396,8 @@ inline void ewise_fused_resnet_block(
     //uint32_t o_h = small::output_dim(in_dims[0] + t_pad_0 + b_pad_0,
     //                                 stride, kernel_size_height);
     uint32_t o_h = in_dims[1];
-    uint32_t o_w = small::output_dim(in_dims[1] + l_pad_0 + r_pad_0,
+    uint32_t o_w = small::output_dim(in_dims[0] + l_pad_0 + r_pad_0,
+
                                      stride, kernel_size_width);
 
     // small::ReLUActivation(output_channels,
@@ -410,7 +412,7 @@ inline void ewise_fused_resnet_block(
     // small::Conv2D(1, stride,
     //               0, 0,
     //               output_channels, input_channels,
-    //               in_dims[0], in_dims[1],
+    //               in_dims[1], in_dims[0],
     //               I, F_conv_1x1, O);
     // #if TIME_LAYER
     // my_timer.stop();
@@ -482,7 +484,7 @@ inline void resnet_block(
     //uint32_t o_h = small::output_dim(in_dims[0] + t_pad_0 + b_pad_0,
     //                                 stride, kernel_size_height);
     uint32_t o_h = in_dims[1];
-    uint32_t o_w = small::output_dim(in_dims[1] + l_pad_0 + r_pad_0,
+    uint32_t o_w = small::output_dim(in_dims[0] + l_pad_0 + r_pad_0,
                                      stride, kernel_size_width);
 #if TIME_LAYER
     my_timer.start();
@@ -1170,12 +1172,10 @@ void inference(uint32_t const n_blocks = 2,
 #if defined(NANO33BLE)
     small::detail::free_all();
 #else
-for (size_t l = 0; l < num_filters; l++)
-{
-    free(filter_buf_ptrs[l]);
-}
-
-
+    for (size_t l = 0; l < num_filters; l++)
+    {
+        small::free_buffer(filter_buf_ptrs[l]);
+    }
 #endif
 }
 
