f87f645af7fe547cbb2ec4f207b57fe225e4b65f
diacccc <935433034@qq.com>
2025-05-23 14:42:27 -0400

    Test for leaky kernel

--- test/test_leaky.cpp
+++ test/test_leaky.cpp
@@ -28,11 +28,60 @@
 
 std::string const data_dir("../test/regression_data");
 
+namespace small {
+namespace float_detail {
+
+void test_correctness_FLOAT_FUSED_COND_SCALE_TILE(void)
+{
+#if defined(SMALL_HAS_FLOAT_SUPPORT)
+    using BufferT = FloatBuffer;
+
+    size_t const INPUT_SIZE = FLOAT_W_ob * FLOAT_C_ib;
+    BufferT input_buf(INPUT_SIZE);
+    for (size_t ix = 0; ix < INPUT_SIZE; ++ix) input_buf[ix] = 2.0 * ((float)rand() / RAND_MAX) - 1;
+
+    float const negative_slope = 0.01;
+
+    const float * negative_buf = &negative_slope;
+
+    size_t const OUTPUT_SIZE = FLOAT_W_ob * FLOAT_C_ob;
+    BufferT output_buf(OUTPUT_SIZE);
+    for (size_t ix = 0; ix < OUTPUT_SIZE; ++ix) output_buf[ix] = input_buf[ix];
+
+    std::cout << std::endl;
+
+
+    //==================================================
+    FLOAT_DEF_TILE_C(FLOAT_W_ob, FLOAT_C_ob);
+
+    FLOAT_LOAD_TILE_C(output_buf.data(), FLOAT_W_ob, FLOAT_C_ob);
+
+    FLOAT_FUSED_COND_SCALE_TILE_C(negative_buf, FLOAT_W_ob, FLOAT_C_ob);
+
+    FLOAT_STORE_TILE_C(output_buf.data(), FLOAT_W_ob, FLOAT_C_ob);
+    //==================================================
+
+    for (dim_t ii = 0; ii < FLOAT_W_ob; ++ii)
+    {
+        for (dim_t jj = 0; jj < FLOAT_C_ob; ++jj)
+        {
+            size_t ix = ii*FLOAT_C_ob + jj;
+            std::cout << output_buf[ix] << " " << ((output_buf[ix] >= 0) ? (input_buf[ix]) : (negative_slope*input_buf[ix])) << " " << input_buf[ix] << " " << ix << std::endl;
+            TEST_CHECK((output_buf[ix] >= 0) ? (output_buf[ix] == input_buf[ix]) : (output_buf[ix] == negative_slope*input_buf[ix]));
+        }
+    }
+#endif 
+}
+
+}
+}
+
 //****************************************************************************
 template <class BufferT>
 BufferT create_leaky_data(size_t num_elements)
 {
-    std::default_random_engine generator;
+    auto seed = std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    std::default_random_engine generator(seed);
     std::normal_distribution<float> distribution{0.f, 1.f};  // what distribution is Torch::Tensor::randn?
 
     BufferT input_dc(num_elements);
@@ -55,7 +104,7 @@ void test_leaky_single_element(void)
     size_t const C_i = 16;
     size_t const H = 1;
     size_t const W = 1;
-    int const negative_slope = 0.01;
+    float const negative_slope = 0.01;
     //size_t const kernel_size = 1;
     //size_t const stride = 1;
     //char const type = 'v';
@@ -87,8 +136,8 @@ void test_leaky_single_element(void)
         TEST_CHECK((input_dc[ix] >= 0) ?
                    (output_dc[ix] == input_dc[ix]) :
                    (output_dc[ix] == negative_slope*input_dc[ix]));
-        //std::cout << ix << ": leaky(" << (int)input_dc[ix] << ")-->"
-        //          << (int)output_dc[ix] << std::endl;
+        // std::cout << ix << ": leaky(" << input_dc[ix] << ")-->"
+        //          << output_dc[ix] << std::endl;
     }
 }
 
@@ -98,7 +147,7 @@ void test_leaky_single_tile(void)
     size_t const C_i = 16;
     size_t const H = 1;
     size_t const W = 6;
-    int const negative_slope = 0.01;
+    float const negative_slope = 0.01;
     //size_t const kernel_size = 1;
     //size_t const stride = 1;
     //char const type = 'v';
@@ -512,11 +561,12 @@ void measure_leaky_performance(void)
 //****************************************************************************
 //****************************************************************************
 TEST_LIST = {
+    {"leaky_correctness", small::float_detail::test_correctness_FLOAT_FUSED_COND_SCALE_TILE},
     {"leaky_single_element",  test_leaky_single_element},
     {"leaky_single_tile",  test_leaky_single_tile},
     {"leaky_large_tile",  test_leaky_large_tile},
     // {"leaky_regression_data", test_leaky_regression_data},
     // {"leaky_layer_regression_data", test_leaky_layer_regression_data},
-    // {"leaky_performance", measure_leaky_performance},
+    {"leaky_performance", measure_leaky_performance},
     {NULL, NULL}
 };
