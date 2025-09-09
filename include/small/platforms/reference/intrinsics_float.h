fabb54735c49f1fe5987a0621b68c9d21f6c10c7
Upasana Sridhar <upasanas@andrew.cmu.edu>
2025-09-08 20:38:49 -0400

    added documentation to kernels

--- include/small/platforms/reference/intrinsics_float.h
+++ include/small/platforms/reference/intrinsics_float.h
@@ -135,6 +135,9 @@ namespace small
  *
  * @param W_ob The width of the tile.
  * @param C_ob The number of channels in the tile.
+ * 
+ * form : O (output) -> W_ob x C_ob matrix 
+ *  O[i][j] = 0 for i in [0, W_b), for j in [0,C_ob)
  */
 #define FLOAT_ZERO_TILE_C(W_ob, C_ob)          \
     for (uint32_t kk = 0; kk < W_ob; kk++)     \
@@ -163,6 +166,16 @@ namespace small
 //****************************************************************************
 // Loads
 //****************************************************************************
+/**
+ * @brief Macro to load a tile of values into a (previously defined) tile of C matrix with given dimensions.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * 
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: Elements in I are contiguous, O is in registers
+ *  O[i][j] = I[i][j] for i in [0, W_b), for j in [0,C_ob)
+ */
 
 #define FLOAT_LOAD_TILE_C(O, W_ob, C_ob)                \
     for (uint32_t kk = 0; kk < W_ob; kk++)              \
@@ -189,6 +202,18 @@ namespace small
 //****************************************************************************
 
 // strided loads
+/** 
+ * @brief Macro to load a tile of values into a (previously defined) tile of C matrix with given dimensions.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param step The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: rows in I are not contiguous, O is in registers
+ * O[i][j] = I[i*step][j] for i in [0, W_b), for j in [0,C_ob)
+*/
+/** @todo step should be stride? We have an implicit assumption that C_ob is the fastest dimension
+The code below expects step = stride*C_ib */
 #define FLOAT_LOAD_TILE_C_strided(O, step, W_ob, C_ob)         \
     for (uint32_t kk = 0; kk < W_ob; kk++)                     \
     {                                                          \
@@ -212,7 +237,18 @@ namespace small
 //****************************************************************************
 // Upsampling loads (stride < 1)
 //****************************************************************************
-
+/** 
+ * @brief Macro to load a tile of values into a (previously defined) tile of C matrix with given dimensions.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive rational between 0 and 1, passed as an integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: rows in I are not contiguous, O is in registers
+ * O[i][j] = I[i*stride][j] for i in [0, W_b), for j in [0,C_ob)
+ * implemented as
+ * O[i][j] = I[i/(stride)][j] for i in [0, W_b), for j in [0,C_ob)
+*/
 #define FLOAT_LOAD_TILE_C_upsample(I, stride, _C_ib, W_ob, C_ob)        \
     for (uint32_t kk = 0; kk < W_ob; kk++)                              \
     {                                                                   \
@@ -233,9 +269,48 @@ namespace small
 
 
 //****************************************************************************
+// Missing Loads within the notation
+//****************************************************************************
+/** 
+ * @brief Macro to broadcast a vector of values into a (previously defined) tile of C matrix with given dimensions.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * layout: Elements in I are contiguous, O is in registers 
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x 1 matrix
+ * O[i][j] = I[i] for i in [0, W_b), for j in [0,C_ob)
+
+ OR
+
+  * form : O (output) -> W_ob x C_ob matrix, I (input) -> 1 x C_ob matrix
+ * O[i][j] = I[j] for i in [0, W_b), for j in [0,C_ob)
+*/
+
+/** 
+ * @brief Macro to broadcast a scalar into a (previously defined) tile of C matrix with given dimensions.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * layout: O is in registers 
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> 1 x 1 matrix
+ * O[i][j] = I[0][0] for i in [0, W_b), for j in [0,C_ob)
+*/
+
+
+//****************************************************************************
 // Stores
 //****************************************************************************
 
+
+/** 
+ * @brief Macro to store a tile of local values to a pointer
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: I is in registers, O is contiguous
+ * O[i][j] = I[i[j] for i in [0, W_b), for j in [0,C_ob)
+*/
 #define FLOAT_STORE_TILE_C(O, W_ob, C_ob)               \
     for (uint32_t kk = 0; kk < W_ob; kk++)              \
     {                                                   \
@@ -253,12 +328,37 @@ namespace small
             O[kk * C_ob + jj] = c_tile[kk * C_ob + jj]; \
         }                                               \
     }
-
+//****************************************************************************
+// Missing Stores within notation
+//****************************************************************************
+/** 
+ * @brief Macro to store a tile of local values to a pointer
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: I is in registers, O is non contiguous
+ * O[i*stride][j] = I[i][j] for i in [0, W_b), for j in [0,C_ob)
+*/
 
 //****************************************************************************
 // Convolution
 //****************************************************************************
 
+/** 
+ * @brief Macro to compute a Rank-UNROLL outer product of I and W, accumulate 
+          with a local output tile, O
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x UNROLL matrix, B(weight) -> UNROLL x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = I[i*stride][k]* W[k][k] + O[i][j] for i in [0, W_b), for j in [0,C_ob), for k in [0, UNROLL)
+*/
+
+/** @todo: UNROLL is 1 for this platform, so it was removed. Add back? */
 #define FLOAT_CONV_TILE_C(step, a, b, W_ob, C_ob)       \
     c_tile_t *c_pixel = c_tile;                         \
     c_tile_t const *a_channel = a;                      \
@@ -299,9 +399,38 @@ namespace small
 
 
 //****************************************************************************
+// Missing Convolution
+//****************************************************************************
+
+
+/** 
+ * @brief Macro to compute a Rank-UNROLL outer product of I and W, accumulate 
+          with a local output tile, O
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive rational between 0 < 1, passed as integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x UNROLL matrix, B(weight) -> UNROLL x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] += I[i/stride][k]* W[k][k] for i in [0, W_b), for j in [0,C_ob), for k in [0, UNROLL)
+*/
+
+
+//****************************************************************************
 // Pooling
 //   Max pooling
 //****************************************************************************
+
+/** 
+ * @brief Macro to compute a tile of the maximum of the local tile O, and an input I
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = max(I[i*stride][j],O[i][j])  for i in [0, W_b), for j in [0,C_ob)
+*/
 
 #define FLOAT_MAX_TILE_C(step, a, W_ob, C_ob)                           \
     c_tile_t *c_pixel = c_tile;                                         \
@@ -338,9 +467,48 @@ namespace small
     }
 
 
+/** 
+ * @brief Macro to compute a tile of the maximum of the local tile O, and an input I
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> 1 x 1 matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = max(I[0][0],O[i][j])  for i in [0, W_b), for j in [0,C_ob)
+//FUSED MAX
+
+AND
+
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x 1 matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = max(I[i*stride][0],O[i][j])  for i in [0, W_b), for j in [0,C_ob)
+
+AND
+
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> 1 x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = max(I[0][C_ob],O[i][j])  for i in [0, W_b), for j in [0,C_ob)
+
+*/
+
+
+
 //****************************************************************************
 // DW Convolution
 //****************************************************************************
+/** 
+ * @brief Macro to compute the element-wise product of two matrices I and 
+          W (rank promoted),
+          accumulate with local output tile.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix, W -> 1 x C_ob 
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = (I[i*stride][j] * W[0][j]) + O[i][j]  for i in [0, W_b), for j in [0,C_ob)
+*/
 
 #define FLOAT_DW_TILE_C(step, a, b, W_ob, C_ob)                 \
     {                                                           \
@@ -384,6 +552,19 @@ namespace small
         }                                                       \
     }
 
+
+/** 
+ * @brief Macro to compute the element-wise product of two matrices I and W,
+          accumulate with local output tile.
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix, W -> W_ob x C_ob 
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = (I[i*stride][j] * W[i][j]) + O[i][j] for i in [0, W_b), for j in [0,C_ob)
+*/
+
 //****************************************************************************
 // ReLU Activation
 //****************************************************************************
@@ -391,6 +572,16 @@ namespace small
 // Same kernel as Pooling, set to zero to start.
 
 // When Fused, compare with a register of zeros
+/** 
+ * @brief Macro to compute a tile of the maximum of the local tile O, and an input I
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output/input) -> W_ob x C_ob matrix, I (input) -> 1 x 1 matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = max(I[0][0],O[i][j])  for i in [0, W_b), for j in [0,C_ob)
+ */
 #define FLOAT_FUSED_RELU_TILE_C(W_ob, C_ob)                           \
     float *c_pixel = c_tile;                                          \
     for (uint32_t kk = 0; kk < W_ob; kk++)                            \
@@ -419,6 +610,16 @@ namespace small
 //****************************************************************************
 // Leaky ReLU activation
 //****************************************************************************
+
+/** 
+ * @brief Macro to compute a tile of a piecewise linear function
+
+    O[i][j] = I[i][j] if I[i][j] > O[i][j]
+    O[i][j] = I[i][j]*W[0][0] if I[i][j] <= O[i][j]       
+    
+    for all i in [0, W_ob), for j in [0,C_ob)
+
+ */
 
 #define FLOAT_COND_SCALE_TILE_C(step, a, b, W_ob, C_ob)                 \
     c_tile_t *c_pixel = c_tile;                                         \
@@ -456,6 +657,19 @@ namespace small
         c_pixel += C_ob;                                                \
     }
 
+
+/** 
+ * @brief Macro to compute a tile of a piecewise linear function
+
+    O[i][j] = O[i][j] if I[0][0] < O[i][j]
+    O[i][j] = O[i][j]*W[0][0] if I[0][0] >= O[i][j]       
+    
+    for all i in [0, W_ob), for j in [0,C_ob)
+
+ */
+
+
+
 #define FLOAT_FUSED_COND_SCALE_TILE_C(b, W_ob, C_ob)                                       \
     float *c_pixel = c_tile;                                                               \
     float scale = b[0];                                                                    \
@@ -487,7 +701,16 @@ namespace small
 //****************************************************************************
 // Accumulation kernels
 //****************************************************************************
-
+/** 
+ * @brief Macro to accumulate a tile of an input I, onto the local tile O
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = I[i*stride][j] + O[i][j] for i in [0, W_b), for j in [0,C_ob)
+ */
 #define FLOAT_ACCUM_TILE_C(step, a, W_ob, C_ob) \
     float *c_pixel = c_tile;                    \
     float const *a_pixel = a;                   \
@@ -530,7 +753,15 @@ namespace small
 //****************************************************************************
 // Broadcast multiplication kernels
 //****************************************************************************
-
+/** 
+ * @brief Macro to scale every element of a tile by a constant value
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> 0 x 0 matrix
+ * layout: O is in registers
+ * O[i][j] = O[i][j] * I[0][0]  for i in [0, W_b), for j in [0,C_ob)
+ */
 #define FLOAT_DIV_TILE_C(norm, W_ob, C_ob)     \
     float *c_pixel = c_tile;                   \
     for (uint32_t kk = 0; kk < W_ob; kk++)     \
@@ -560,6 +791,20 @@ namespace small
 //****************************************************************************
 // Accumulate upsampling
 //****************************************************************************
+
+/** 
+ * @brief Macro to accumulate a tile of an input I, onto the local tile O
+ *
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive rational between 0 and 1
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = I[i*stride][j] + O[i][j] for i in [0, W_b), for j in [0,C_ob)
+ * implemented as
+  * O[i][j] = I[i/stride][j] + O[i][j] for i in [0, W_b), for j in [0,C_ob)
+ */
+
 #define FLOAT_ACCUM_TILE_C_upsample(I, stride, _C_ib, _W_ob, C_ob)     \
     for (uint32_t kk = 0; kk < _W_ob; kk++)                            \
     {                                                                  \
@@ -581,6 +826,19 @@ namespace small
 //****************************************************************************
 // Accumulate channel dimension
 //****************************************************************************
+
+
+/** 
+ * @brief Macro to accumulate all elements of the local tile O
+ *
+ * @param O_w_left The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * form : O (output) -> 1 x 1 matrix, O(input) -> 1 x C_ob matrix
+ * layout: O is in registers
+ * O[0] = O[0] + O[j] for j in [1,C_ob)
+ * and
+ * O[j] = 0 for j in [1,C_ob)
+ */
 
 #define FLOAT_REDUCE_CHANNEL_END_C(O_w_left, C_ob)                      \
     if constexpr (C_ob == 1 && C_ob != FLOAT_SIMD_EPILOGUE)             \
@@ -668,6 +926,16 @@ namespace small
 // Softmax  (Ewise exponentiation)
 //****************************************************************************
 
+/** 
+ * @brief Macro to compute a tile of exponentiated values in I
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output) -> W_ob x C_ob matrix, I (input) -> W_ob x C_ob matrix
+ * layout: O is in registers, I and W are row-major, Rows of I need not be contiguous
+ * O[i][j] = e ^(I[i*stride][j]) for i in [0, W_b), for j in [0,C_ob)
+*/
+
 #define FLOAT_EXP_TILE_C(step, a, W_ob, C_ob)                           \
     c_tile_t *c_pixel = c_tile;                                         \
     c_tile_t const *a_pixel = a;                                        \
@@ -702,6 +970,16 @@ namespace small
         c_pixel += C_ob;                              \
     }
 
+
+/** 
+ * @brief Macro to compute a tile of exponentiated values in the local tile O
+ * @param W_ob The width of the tile.
+ * @param C_ob The number of channels in the tile.
+ * @param stride The stride in between rows of I, positive integer
+ * form : O (output, input) -> W_ob x C_ob matrix
+ * layout: O is in registers
+ * O[i][j] = e ^(O[i][j]) for i in [0, W_b), for j in [0,C_ob)
+*/    
 #define FLOAT_FUSED_EXP_TILE_C(W_ob, C_ob)       \
     c_tile_t *c_pixel = c_tile;                  \
     for (uint32_t kk = 0; kk < W_ob; kk++)       \
