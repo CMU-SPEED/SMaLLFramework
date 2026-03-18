862eca3a9ec5c650aea612b0f45a97d48535abc2
UpasanaSridhar <upasanas@cmu.edu>
2026-03-18 16:07:58 -0400

    using inplace relu in fused driver

--- demo/fused_block_driver.cpp
+++ demo/fused_block_driver.cpp
@@ -219,7 +219,7 @@ inline void small_layer_block(
     my_timer.start();
     if constexpr (COMPUTE_RELU)
     {
-    small::ReLUActivation(output_channels_conv, o_h, o_w, O_intermediate, O_intermediate);
+    small::ReLUActivation_inplace(output_channels_conv, o_h, o_w, O_intermediate);
     }
     my_timer.stop();
     layer_timers[0][1] = my_timer.elapsed();
