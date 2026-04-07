b7c23f3cc120898841f19c63ac20380323cba5d2
Yufei Shi <86854844+yshi02@users.noreply.github.com>
2026-04-07 17:22:03 -0400

    [float] Format the abstract kernel generator

--- include/small/float_detail/float_abstract_kernel_generator.py
+++ include/small/float_detail/float_abstract_kernel_generator.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 from collections import OrderedDict
@@ -24,42 +23,40 @@ Will generate the Compute phase with the right dataflow
 
 """
 
-#platform specific setup
+# platform specific setup
 
 
-
-
-vector_instruction_table={ 
-# memory operations    
-"load":"_mm256_load_ps({ptr})",
-"store": "_mm256_store_ps({ptr}, {reg})",
-"broadcast": "_mm256_broadcast_ss({ptr})",
-# 0 input operands
-"zero": "_mm256_setzero_ps()",
-# 1 input operand
-"assign":"{reg1}",
-# binary operands
-"add": "_mm256_add_ps({reg1}, {reg2})",
-"sub": "_mm256_sub_ps({reg1}, {reg2})",
-"mul" :"_mm256_mul_ps({reg1}, {reg2})",
-"min": "_mm256_min_ps({reg1}, {reg2})",
-"max": "_mm256_max_ps({reg1}, {reg2})",
-"and": "_mm256_and_ps({reg1}, {reg2})",
-"gt": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_GT_OQ)",
-"lt": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_LT_OQ)",
-"ge": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_GE_OQ)",
-"le": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_LE_OQ)",
-"eq": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_EQ_OQ)",
-# ternary operands
-"fma" :"_mm256_fmadd_ps({reg1}, {reg2}, {reg3})",
-"fmaddsub": "_mm256_fmaddsub_ps({reg1}, {reg2}, {reg3})",
-# architecture specific details
-"vector_type": "__m256",
-"scalar_type": "float",
-"REGS": 16,
-"VECTOR_BITS": 256,
-"ELEMENT_BITS": 32,
- "SIMD":8
+vector_instruction_table = {
+    # memory operations
+    "load": "_mm256_load_ps({ptr})",
+    "store": "_mm256_store_ps({ptr}, {reg})",
+    "broadcast": "_mm256_broadcast_ss({ptr})",
+    # 0 input operands
+    "zero": "_mm256_setzero_ps()",
+    # 1 input operand
+    "assign": "{reg1}",
+    # binary operands
+    "add": "_mm256_add_ps({reg1}, {reg2})",
+    "sub": "_mm256_sub_ps({reg1}, {reg2})",
+    "mul": "_mm256_mul_ps({reg1}, {reg2})",
+    "min": "_mm256_min_ps({reg1}, {reg2})",
+    "max": "_mm256_max_ps({reg1}, {reg2})",
+    "and": "_mm256_and_ps({reg1}, {reg2})",
+    "gt": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_GT_OQ)",
+    "lt": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_LT_OQ)",
+    "ge": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_GE_OQ)",
+    "le": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_LE_OQ)",
+    "eq": "_mm256_cmp_ps({reg1}, {reg2}, _CMP_EQ_OQ)",
+    # ternary operands
+    "fma": "_mm256_fmadd_ps({reg1}, {reg2}, {reg3})",
+    "fmaddsub": "_mm256_fmaddsub_ps({reg1}, {reg2}, {reg3})",
+    # architecture specific details
+    "vector_type": "__m256",
+    "scalar_type": "float",
+    "REGS": 16,
+    "VECTOR_BITS": 256,
+    "ELEMENT_BITS": 32,
+    "SIMD": 8,
 }
 
 # Mapping from operator to number of operands
@@ -79,23 +76,25 @@ operator_operand_count = {
     "le": 2,
     "eq": 2,
     "fma": 3,
-    "fmaddsub": 3
+    "fmaddsub": 3,
 }
 
 
-W_ob = int(sys.argv[1].split("x")[0]) #should be of the form w:c
+W_ob = int(sys.argv[1].split("x")[0])  # should be of the form w:c
 C_ob = int(sys.argv[1].split("x")[1])
-c_regs = W_ob*C_ob//(vector_instruction_table["SIMD"])
-assert((c_regs) < vector_instruction_table["REGS"])
+c_regs = W_ob * C_ob // (vector_instruction_table["SIMD"])
+assert (c_regs) < vector_instruction_table["REGS"]
 
 
-#operation set up
-vector_instruction = vector_instruction_table[sys.argv[2]] # One of the Keys in the vector instruction table
-name = sys.argv[3] # meaningful name for the operation
+# operation set up
+vector_instruction = vector_instruction_table[
+    sys.argv[2]
+]  # One of the Keys in the vector instruction table
+name = sys.argv[3]  # meaningful name for the operation
 
 num_operands = operator_operand_count[sys.argv[2]]
-#operand shapes
-# operands can appear in any order after the first 3 arguments a, b, c 
+# operand shapes
+# operands can appear in any order after the first 3 arguments a, b, c
 order_of_operands = OrderedDict()
 i = 0
 for arg in sys.argv[4:]:
@@ -106,34 +105,33 @@ for arg in sys.argv[4:]:
         if i >= num_operands:
             break
         if arg.startswith("a:"):
-            shape_a = arg.split(":")[1].split(",") #should be of the form a:w,c
+            shape_a = arg.split(":")[1].split(",")  # should be of the form a:w,c
         elif arg.startswith("b:"):
-            shape_b = arg.split(":")[1].split(",") #should be of the form b:w,c
+            shape_b = arg.split(":")[1].split(",")  # should be of the form b:w,c
         elif arg.startswith("c:"):
-            shape_c_input = arg.split(":")[1].split(",") #should be of the form c:w,c
+            shape_c_input = arg.split(":")[1].split(",")  # should be of the form c:w,c
         order_of_operands[arg[0]] = arg.split(":")[1].split(",")
-        i+=1
+        i += 1
 
 
-if(len(sys.argv) - 4 > num_operands):
+if len(sys.argv) - 4 > num_operands:
     print("Too many operand shape arguments provided for the selected operation")
     print("Ignoring arguments after {:}".format(sys.argv[4 + num_operands]))
 
-#insert default shapes if not provided
+# insert default shapes if not provided
 
 print(order_of_operands.keys())
-if 'a' not in order_of_operands.keys():
+if "a" not in order_of_operands.keys():
     shape_a = None
-    order_of_operands['a'] = shape_a
-if 'b' not in order_of_operands.keys():
+    order_of_operands["a"] = shape_a
+if "b" not in order_of_operands.keys():
     shape_b = None
-    order_of_operands['b'] = shape_b
-if 'c' not in order_of_operands.keys(): 
+    order_of_operands["b"] = shape_b
+if "c" not in order_of_operands.keys():
     shape_c_input = None
     print("no input c")
-    order_of_operands['c'] = shape_c_input
+    order_of_operands["c"] = shape_c_input
 shape_c = [W_ob, C_ob]
-
 
 
 operand_order = list(order_of_operands.keys())
@@ -144,27 +142,36 @@ print(shape_a, shape_b, shape_c_input)
 
 
 def redefine(name):
-    return ['#ifdef {n}\n#undef {n}\n#endif\n'.format(n=name)]
+    return ["#ifdef {n}\n#undef {n}\n#endif\n".format(n=name)]
+
 
 if shape_c_input != None:
-    #If shape_c_input is not the same as W_ob, C_ob then we need to call the appropriate LOAD_FUNCTION_for C
+    # If shape_c_input is not the same as W_ob, C_ob then we need to call the appropriate LOAD_FUNCTION_for C
     l = []
     if int(shape_c_input[1]) == 1:
         c_load = vector_instruction_table["broadcast"]
-        type_c ="VECTOR"
+        type_c = "VECTOR"
         if int(shape_c_input[0]) == 1:
-            type_c ="SCALAR"
-        
+            type_c = "SCALAR"
+
     else:
         c_load = vector_instruction_table["load"]
-        type_c ="VECTOR_T"
-        if int(shape_c_input[0])!= 1:
-            type_c ="MATRIX" 
+        type_c = "VECTOR_T"
+        if int(shape_c_input[0]) != 1:
+            type_c = "MATRIX"
 
-    l += [f'#define FLOAT_LOAD_{type_c}_TILE_C(I, step)\\']    
+    l += [f"#define FLOAT_LOAD_{type_c}_TILE_C(I, step)\\"]
     for kk in range(W_ob):
-        for jj in range(C_ob//vector_instruction_table["SIMD"]):
-            l += [f'c_{kk}_{jj} = ' + c_load.format(ptr=(f"c + {kk%(int(shape_c_input[0]))}*step + {jj%(int(shape_c_input[1]))} * SIMD")) + ";\\"]
+        for jj in range(C_ob // vector_instruction_table["SIMD"]):
+            l += [
+                f"c_{kk}_{jj} = "
+                + c_load.format(
+                    ptr=(
+                        f"c + {kk%(int(shape_c_input[0]))}*step + {jj%(int(shape_c_input[1]))} * SIMD"
+                    )
+                )
+                + ";\\"
+            ]
 
     print("\n".join(l))
 
@@ -173,53 +180,53 @@ if shape_c_input != None:
 
 
 a_load = " "
-#if we need to rank promote the channels, load of a is a broadcast
-#allocate the remaining a_regs registers to the kk and jj loops in the kernel
+# if we need to rank promote the channels, load of a is a broadcast
+# allocate the remaining a_regs registers to the kk and jj loops in the kernel
 
 # ----------------------------------------
 # Register Allocation
 # ----------------------------------------
-#sort out registers needed for b
+# sort out registers needed for b
 
 b_regs_jj_kk = 0
-type_b ="ACCUM"
+type_b = "ACCUM"
 if shape_b != None:
-    assert(int(shape_b[0])==1) #rank promotion only along C_ob
-    b_regs = C_ob//vector_instruction_table["SIMD"]
-    if (int(shape_b[1]) == 1):
+    assert int(shape_b[0]) == 1  # rank promotion only along C_ob
+    b_regs = C_ob // vector_instruction_table["SIMD"]
+    if int(shape_b[1]) == 1:
         b_load = vector_instruction_table["broadcast"]
         b_regs_jj_kk = 1
-        type_b ="SCALAR"
+        type_b = "SCALAR"
     else:
         b_load = vector_instruction_table["load"]
         b_regs_jj_kk = b_regs
-        type_b ="VECTOR_T"
+        type_b = "VECTOR_T"
 
-    assert(b_regs_jj_kk + c_regs <= vector_instruction_table["REGS"])
+    assert b_regs_jj_kk + c_regs <= vector_instruction_table["REGS"]
 
-#sort out registers needed for a
+# sort out registers needed for a
 a_regs = vector_instruction_table["REGS"] - b_regs_jj_kk - c_regs
 if int(shape_a[1]) == 1:
     a_load = vector_instruction_table["broadcast"]
     a_regs_jj = 1
-    type_a ="VECTOR"
+    type_a = "VECTOR"
     if int(shape_a[0]) == 1:
         a_regs_kk = 1
-        type_a ="SCALAR"
+        type_a = "SCALAR"
     else:
         a_regs_kk = a_regs
-    
+
 else:
     a_load = vector_instruction_table["load"]
     # 1 register per simd width in the channel dimension
-    a_regs_jj = C_ob//vector_instruction_table["SIMD"]
-    type_a ="VECTOR_T"
+    a_regs_jj = C_ob // vector_instruction_table["SIMD"]
+    type_a = "VECTOR_T"
     if int(shape_a[0]) == 1:
         a_regs_kk = 1
     else:
-        a_regs_kk = (a_regs//a_regs_jj)
-        type_a ="MATRIX" 
-    
+        a_regs_kk = a_regs // a_regs_jj
+        type_a = "MATRIX"
+
 a_regs_jj_kk = a_regs_jj * a_regs_kk
 
 print()
@@ -228,10 +235,10 @@ print()
 # Compute Phase Name and Declaration
 # ----------------------------------------
 s = []
-s += [f'#define FLOAT_{name}_{type_a}_{type_b}_TILE_C(step, a']
+s += [f"#define FLOAT_{name}_{type_a}_{type_b}_TILE_C(step, a"]
 if shape_b != None:
     s[-1] += ", b"
-s[-1] += ')\\'
+s[-1] += ")\\"
 # compute
 
 # ----------------------------------------
@@ -243,65 +250,88 @@ a_vector_registers = []
 for kk in range(a_regs_kk):
     for jj in range(a_regs_jj):
         a_vector_registers += [f"a_{kk}_{jj}"]
-a_vec_declaration =  ", ".join(a_vector_registers) + "; \\"
-s += ['{vector_type} '.format(vector_type=vector_type_name) + a_vec_declaration]
+a_vec_declaration = ", ".join(a_vector_registers) + "; \\"
+s += ["{vector_type} ".format(vector_type=vector_type_name) + a_vec_declaration]
 
 if shape_b != None:
     b_vector_registers = []
     for jj in range(b_regs_jj_kk):
         b_vector_registers += [f"b_0_{jj}"]
-    b_vec_declaration =  ", ".join(b_vector_registers) + "; \\"
-    s += ['{vector_type} '.format(vector_type=vector_type_name) + b_vec_declaration ] 
+    b_vec_declaration = ", ".join(b_vector_registers) + "; \\"
+    s += ["{vector_type} ".format(vector_type=vector_type_name) + b_vec_declaration]
 
-order_of_operands['a'] = [a_regs_kk, a_regs_jj]
-order_of_operands['c'] = [W_ob, C_ob]
-order_of_operands['b'] = [1, b_regs_jj_kk]
+order_of_operands["a"] = [a_regs_kk, a_regs_jj]
+order_of_operands["c"] = [W_ob, C_ob]
+order_of_operands["b"] = [1, b_regs_jj_kk]
 
 # ----------------------------------------
 # Input Loading
 # ----------------------------------------
 
 # if a is reused over the width elements, load can be performed outside
-if int(shape_a[0])==1:
+if int(shape_a[0]) == 1:
     for jj in range(a_regs_jj):
         s += [f"a_0_{jj} = " + a_load.format(ptr=(f"a + {jj} * SIMD")) + ";\\"]
 
-#if the b operand is present load it
+# if the b operand is present load it
 if shape_b != None:
     for jj in range(b_regs_jj_kk):
         s += [f"b_0_{jj} = " + b_load.format(ptr=(f"b + {jj} * SIMD")) + ";\\"]
 
 
-
-op_2_j_regs = int(C_ob//vector_instruction_table["SIMD"])
+op_2_j_regs = int(C_ob // vector_instruction_table["SIMD"])
 op_2_k_regs = W_ob
 
-op_1_j_regs = int(C_ob//vector_instruction_table["SIMD"])
+op_1_j_regs = int(C_ob // vector_instruction_table["SIMD"])
 op_1_k_regs = W_ob
 
-reg1_unlabeled = "{op}".format(op= operand_order[0]) + "_{idxs[0][0]}_{idxs[0][1]}"
-reg2_unlabeled = "{op}".format(op= operand_order[1]) + "_{idxs[1][0]}_{idxs[1][1]}" if num_operands >=2 else None
-reg3_unlabeled = "{op}".format(op= operand_order[2]) + "_{idxs[2][0]}_{idxs[2][1]}" if num_operands ==3 else None
+reg1_unlabeled = "{op}".format(op=operand_order[0]) + "_{idxs[0][0]}_{idxs[0][1]}"
+reg2_unlabeled = (
+    "{op}".format(op=operand_order[1]) + "_{idxs[1][0]}_{idxs[1][1]}"
+    if num_operands >= 2
+    else None
+)
+reg3_unlabeled = (
+    "{op}".format(op=operand_order[2]) + "_{idxs[2][0]}_{idxs[2][1]}"
+    if num_operands == 3
+    else None
+)
 
 
-
-vector_instruction_instance = vector_instruction.format(reg1=reg1_unlabeled, reg2 = reg2_unlabeled, reg3=reg3_unlabeled)
+vector_instruction_instance = vector_instruction.format(
+    reg1=reg1_unlabeled, reg2=reg2_unlabeled, reg3=reg3_unlabeled
+)
 
 for kk in range(W_ob):
-    #if a is not reused it must be reloaded
-    if int(shape_a[0])>1 :
+    # if a is not reused it must be reloaded
+    if int(shape_a[0]) > 1:
         for jj in range(a_regs_jj):
-            s += [f"a_{(kk%a_regs_kk)}_{jj} = " + a_load.format(ptr=(f"a + {kk}*step + {jj} * SIMD")) + ";\\"]       
-    for jj in range(C_ob//vector_instruction_table["SIMD"]):
+            s += [
+                f"a_{(kk%a_regs_kk)}_{jj} = "
+                + a_load.format(ptr=(f"a + {kk}*step + {jj} * SIMD"))
+                + ";\\"
+            ]
+    for jj in range(C_ob // vector_instruction_table["SIMD"]):
         # if not (shape_b == None):
         register_operand_idxs = []
         for operand in range(num_operands):
-            register_operand_idxs.append([kk % order_of_operands[operand_order[operand]][0], jj % order_of_operands[operand_order[operand]][1]])
+            register_operand_idxs.append(
+                [
+                    kk % order_of_operands[operand_order[operand]][0],
+                    jj % order_of_operands[operand_order[operand]][1],
+                ]
+            )
 
-        s += ['c_{k}_{j} = {vop};\\'.format(vop=vector_instruction_instance.format(idxs = register_operand_idxs), k=kk, j=jj)]
+        s += [
+            "c_{k}_{j} = {vop};\\".format(
+                vop=vector_instruction_instance.format(idxs=register_operand_idxs),
+                k=kk,
+                j=jj,
+            )
+        ]
         # else:
         #     s += ['c_{k}_{j} = {vop});\\'.format(vop = vector_instruction_instance, k=kk, j=jj, kj=((kk%a_regs_kk)*(a_regs_jj)+(jj%a_regs_jj)))]
-s += ['']
+s += [""]
 print("\n".join(s))
 
 # c_regs_jj = C_ob//vector_instruction_table["SIMD"]
@@ -324,6 +354,3 @@ print("\n".join(s))
 # s+=["}"]
 # s += ['']
 # print("\n".join(s))
-
-
-
